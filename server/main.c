90e78823e3afbf36d98c4564aee6a928e20ec597
Ralfs Braunfelds <ralfsbraunfelds@gmail.com>
2019-12-26 20:16:32 +0200

    Adapting server to new documentation changes

--- server/main.c
+++ server/main.c
@@ -8,34 +8,40 @@
 #include <pthread.h>
 #include <string.h>
 
-#define MAX_CLIENT_COUNT 12
-#define MAX_TYPE_SIZE 2
+#define MAX_TYPE_SIZE 1
 #define MAX_USERNAME_SIZE 16
 #define MAX_PLAYER_COUNT 8
 
-#define R_JOIN_GAME "11"
-#define R_MOVE "12"
-#define S_LOBBY_INFO "21"
-#define S_GAME_START "22"
-#define S_GAME_UPDATE "23"
-#define S_PLAYER_DEAD "24"
-#define S_GAME_END "25"
-#define E_GAME_IN_PROGRESS "31"
-#define E_USERNAME_TAKEN "32"
-#define E_TECHNICAL "33"
+#define R_JOIN_GAME "0"
+#define R_MOVE "1"
+#define S_LOBBY_INFO "2"
+#define S_GAME_START "5"
+#define S_MAP_ROW "6"
+#define S_GAME_UPDATE "7"
+#define S_PLAYER_DEAD "8"
+#define S_GAME_END "9"
+#define E_GAME_IN_PROGRESS "3"
+#define E_USERNAME_TAKEN "4"
+#define E_TECHNICAL "A"
 
-int clients[MAX_CLIENT_COUNT];
-char usernames[MAX_CLIENT_COUNT][MAX_USERNAME_SIZE + 1];
-int connectedClientCount = 0;
+int players[MAX_PLAYER_COUNT];
+char usernames[MAX_PLAYER_COUNT][MAX_USERNAME_SIZE + 1];
+int connectedPlayerCount = 0;
+
+int numPlaces (int n) {
+    if (n < 10) return 1;
+    return 1 + numPlaces (n / 10);
+}
 
 int socketSend(int socket, char *message, int messageSize) {
     int ret = send(socket, message, messageSize, 0);
+    printf("Message sent %s\n", message);
     return ret;
 }
 
 int socketReceive(int socket, char *message, short messageSize) {
     int ret = recv(socket, message, messageSize, 0);
-    printf("Message %s\n", message);
+    printf("Message received %s\n", message);
     return ret;
 }
 
@@ -44,55 +50,55 @@ void respondWithError(int clientSocket, char *errorType) {
     close(clientSocket);
 }
 
-int addClientToList(int clientSocket) {
+int addClientToGame(int clientSocket) {
     int i;
     char joinGameRequest[MAX_TYPE_SIZE + MAX_USERNAME_SIZE + 1] = "";
-    char requestType[3] = "";
+    char requestType[2] = "";
 
-    for (i = 0; i < MAX_CLIENT_COUNT; i++) {
-        if (clients[i] == 0){
+    for (i = 0; i < MAX_PLAYER_COUNT; i++) {
+        if (players[i] == 0){
             socketReceive(clientSocket, joinGameRequest, sizeof(joinGameRequest));
 
-            if (strlen(joinGameRequest) < 3) {
+            if (strlen(joinGameRequest) < 2) {
                 printf("Request is not in the correct format\n");
                 respondWithError(clientSocket, E_TECHNICAL);
+                printf("Cannot add player to game - incorrect join game request size\n");
                 return 0;
             }
 
-            strncpy(requestType, joinGameRequest, 2);
+            strncpy(requestType, joinGameRequest, 1);
 
             if (strcmp(requestType, R_JOIN_GAME) != 0) {
-                printf("Expected request type 11 but was %s\n", requestType);
+                printf("Expected request type %s but was %s\n", R_JOIN_GAME, requestType);
                 respondWithError(clientSocket, E_TECHNICAL);
+                printf("Cannot add player to game - incorrect join game request type\n");
                 return 0;
             }
 
-            strcpy(usernames[i], &joinGameRequest[2]);
-            clients[i] = clientSocket;
-            connectedClientCount++;
+            strcpy(usernames[i], &joinGameRequest[1]);
+            players[i] = clientSocket;
+            connectedPlayerCount++;
 
+            printf("Player successfully added to game\n");
             return 1;
         }
     }
 
+    printf("Cannot add player to game - game full\n");
     return 0;
 }
 
-void notifyLobbyInfo() {
+void sendLobbyInfoToAll() {
     int i;
     char lobbyInfoMessage[MAX_TYPE_SIZE + 1 + MAX_PLAYER_COUNT * MAX_USERNAME_SIZE + 1] = "";
-    sprintf(lobbyInfoMessage, "%s%d", S_LOBBY_INFO, connectedClientCount);
+    sprintf(lobbyInfoMessage, "%s%d", S_LOBBY_INFO, connectedPlayerCount);
 
-    for (i = 0; i < connectedClientCount; i++) {
+    for (i = 0; i < connectedPlayerCount; i++) {
         strcat(lobbyInfoMessage, usernames[i]);
     }
 
-    for (i = 0; i < MAX_CLIENT_COUNT; i++) {
-        if (clients[i] == 0) {
-            return;
-        }
-
-        socketSend(clients[i], lobbyInfoMessage, strlen(lobbyInfoMessage));
+    for (i = 0; i < connectedPlayerCount; i++) {
+        socketSend(players[i], lobbyInfoMessage, strlen(lobbyInfoMessage));
     }
 }
 
@@ -112,11 +118,9 @@ int main() {
         int clientSocket = accept(netSocket, NULL, NULL);
         printf("Client connected\n");
 
-        if (addClientToList(clientSocket)) {
-            printf("Client successfully added to list\n");
-            notifyLobbyInfo();
+        if (addClientToGame(clientSocket)) {
+            sendLobbyInfoToAll();
         } else {
-            printf("Unable to add client, list full\n");
             respondWithError(clientSocket, E_GAME_IN_PROGRESS);
         }
     }
